db2b8d547f48f0a6a605739140272ce27be47957
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-11-06 21:02:36 +0100

    Add fields state to keep data

--- src/app/(public)/signup/form.tsx
+++ src/app/(public)/signup/form.tsx
@@ -4,17 +4,28 @@ import { signup } from '@/actions/auth';
 import { Input } from '@/components/ui/input';
 import { PasswordInput } from '@/components/ui/password-input';
 import { cn } from '@/lib/utils';
-import { useActionState } from 'react';
+import { useActionState, useState } from 'react';
 
 export default function SignupForm() {
   const [state, action, pending] = useActionState(signup, undefined);
+  const [fields, setFields] = useState({ name: '', email: '' });
+
+  function handleChange(e: React.ChangeEvent<HTMLInputElement>) {
+    setFields({ ...fields, [e.target.name]: e.target.value });
+  }
 
   return (
     <form action={action}>
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 text-sm">
         <div className="flex flex-col gap-2">
           <label htmlFor="name">Name</label>
-          <Input id="name" name="name" placeholder="John Doe" />
+          <Input
+            id="name"
+            name="name"
+            placeholder="John Doe"
+            value={fields.name}
+            onChange={handleChange}
+          />
         </div>
         {state?.errors?.name && (
           <p className="text-error">{state.errors.name}</p>
@@ -27,6 +38,8 @@ export default function SignupForm() {
             id="email"
             name="email"
             placeholder="johndoe@example.com"
+            value={fields.email}
+            onChange={handleChange}
           />
         </div>
         {state?.errors?.email && (
@@ -51,7 +64,7 @@ export default function SignupForm() {
         <button
           type="submit"
           disabled={pending}
-          className={cn('px-3 py-2 rounded-md bg-blue-500', {
+          className={cn('px-3 py-2.5 rounded-md bg-blue-500', {
             'opacity-50 cursor-not-allowed': pending,
           })}
         >
