075717d62dd93a6f46cc7e2212ddc9037b089c2a
Bastien Ricard <bastien.ricard-dev@outlook.com>
2024-11-07 10:48:50 +0100

    Add shadcn

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -1,22 +1,25 @@
-import { cn } from '@/lib/utils';
-import * as React from 'react';
+import * as React from "react"
 
-const Input = React.forwardRef<
-  HTMLInputElement,
-  React.InputHTMLAttributes<HTMLInputElement>
->(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      type={type}
-      className={cn(
-        'w-full h-10 px-3 py-2 flex items-center border rounded-md  bg-transparent text-sm focus-visible:outline-blue-500 focus-visible:outline-none',
-        className
-      )}
-      ref={ref}
-      {...props}
-    />
-  );
-});
-Input.displayName = 'Input';
+import { cn } from "@/lib/utils"
 
-export { Input };
+export interface InputProps
+  extends React.InputHTMLAttributes<HTMLInputElement> {}
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ className, type, ...props }, ref) => {
+    return (
+      <input
+        type={type}
+        className={cn(
+          "flex h-10 w-full rounded-md border border-input bg-background px-3 py-2 text-base ring-offset-background file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+          className
+        )}
+        ref={ref}
+        {...props}
+      />
+    )
+  }
+)
+Input.displayName = "Input"
+
+export { Input }
